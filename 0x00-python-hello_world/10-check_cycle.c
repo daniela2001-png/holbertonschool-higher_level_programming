b9ac3749a0317568a94e1421f3939fd732ab7ef3
daniela2001-png <dmbmora@outlook.com>
2020-08-31 13:41:50 -0500

    floyd

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * check_cycle - validate if a SLK had a loop
+ * @list: is the single linked list
+ * Return: 0 if not found a loop else return 1
+ */
+
+
+int check_cycle(listint_t *list)
+{
+
+	listint_t *slow = NULL;
+	listint_t *quick = NULL;
+
+	slow = list->next;
+	quick = list->next->next;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	while (quick != NULL && slow != NULL && quick->next != NULL)
+	{
+		if (quick == slow)
+			return (1);
+		slow = slow->next;
+		quick = quick->next->next;
+	}
+	return (0);
+}
