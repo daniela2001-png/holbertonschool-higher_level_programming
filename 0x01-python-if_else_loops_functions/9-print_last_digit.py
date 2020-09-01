839db726df11a89de6a805b7191736c8012ef992
daniela2001-png <dmbmora@outlook.com>
2020-09-01 14:52:26 -0500

    submit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def print_last_digit(number):
+    last_digit = abs(number) % 10
+    if (number > 0):
+        print('{:d}'.format(last_digit), end='')
+        return(last_digit)
+    else:
+        print('{:d}'.format(last_digit), end='')
+        return(last_digit)
