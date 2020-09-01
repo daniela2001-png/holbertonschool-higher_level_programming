8f227ed4e3d0f660936bb8e14e341201ad434ab8
daniela2001-png <dmbmora@outlook.com>
2020-09-01 14:34:43 -0500

    submit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,8 @@
 def uppercase(str):
     for i in str:
         if (ord(i) >= ord('a') and ord(i) <= ord('z')):
-            print(chr(ord(i) - 32))
+            mayuscula = (chr(ord(i) - 32))
         else:
-            print(chr(ord(i)))
+            mayuscula = i
+        print('{:s}'.format(mayuscula), end='')
+    print()
