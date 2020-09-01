eed58193d52030f3389e8ebfe4d36edb47e2c666
daniela2001-png <dmbmora@outlook.com>
2020-09-01 14:16:28 -0500

    submit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for each_letter in range(97, 123):
     if (each_letter == 101 or each_letter == 113):
         continue
-    print(chr(each_letter), end='')
+    print('{:s}'.format(chr(each_letter)), end='')
