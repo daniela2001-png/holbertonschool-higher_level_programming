5e00390b97de3fe575c1eab78d34cf54490b9fd0
daniela2001-png <dmbmora@outlook.com>
2020-09-08 14:30:39 -0500

    data structures with python3

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,9 @@
 def no_c(my_string):
     my_list = list(my_string)
     for letter in my_list:
-        if (letter == 'c' or letter == 'C'):
+        if (letter == 'c'):
             my_list.remove(letter)
-        str_join = "".join(my_list)
-        return (str_join)
+        if (letter == 'C'):
+            my_list.remove(letter)
+    str_join = "".join(my_list)
+    return (str_join)
