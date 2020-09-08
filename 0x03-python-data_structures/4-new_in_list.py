3dab1097e21369c5a2d18e8ab6a75ccd636580d1
daniela2001-png <dmbmora@outlook.com>
2020-09-08 14:12:54 -0500

    data structures with python3

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    cpy_list = my_list[:]
-    for i in my_list:
-        if (idx < 0):
-            return (cpy_list)
-        if (idx > len(my_list)):
-            return (cpy_list)
-        if (idx == cpy_list.index(i)):
-            cpy_list.pop(idx)
-            cpy_list.insert(idx, element)
-            return (cpy_list)
+    if (my_list):
+        cpy_list = my_list[:]
+        for i in my_list:
+            if (idx < 0):
+                return (cpy_list)
+            if (idx >= len(my_list)):
+                return (cpy_list)
+            if (idx == cpy_list.index(i)):
+                cpy_list.pop(idx)
+                cpy_list.insert(idx, element)
+                return (cpy_list)
