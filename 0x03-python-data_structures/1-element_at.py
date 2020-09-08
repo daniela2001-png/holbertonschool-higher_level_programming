ed42a87b4ad61b2b570105d40ded3b3fab49667d
daniela2001-png <dmbmora@outlook.com>
2020-09-08 12:58:46 -0500

    data structures with python3

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    for i in my_list:
+        if (idx < 0):
+            return (None)
+        if (idx > len(my_list)):
+            return (None)
+        if (idx == my_list.index(i)):
+            return (i)
