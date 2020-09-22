bb23d8366362b7cd758fa4ca36fc145a442d7674
daniela2001-png <dmbmora@outlook.com>
2020-09-22 16:11:09 -0500

    add square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
 """
-define a private instance object
-and add a new method instance area
+    this class is empty
 """
 
 
 class Square:
     """
-    define init and new method
+        this class contains a unique private
+        attribute because the theme of
+        perimeter areas of a square is essential
+
+
     """
 
-    def __init__(self, size):
-        """
-        evaluo los errores
-        """
+    def __init__(self, size=0):
+        """This init contains:
+    instance private attribute (object)
+
+    it is called: size and is a integer
+
+    Return Value: None """
         if (type(size) is not int):
             raise TypeError("size must be an integer")
-        elif (size < 0):
+        elif(size < 0):
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+# size is the value of the instance
+# Instead be a instance!
 
     def area(self):
-        """
-        retorno el area del cuadrado
-        """
-        return (self.__size)**2
+        """Method od Instance public that takes a object"""
+        return(self.__size) ** 2
