1846b23c5feec84b73f9a0f9e25e38179ae5d65b
daniela2001-png <dmbmora@outlook.com>
2020-09-25 23:57:57 -0500

    adding file matrix sublist and divide

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,20 +1,34 @@
 #!/usr/bin/python3
+"""
+Function that divides a matrix (list with sublist)
+each number shoul be a integer or float
+"""
+
+
 def matrix_divided(matrix, div):
-    if type(matrix) is list:
-        for element in matrix:
-            if (type(element) is not list):
-                raise TypeError(
-                    "matrix must be a matrix (list of lists) of integers/floats")
-            else:
-                for number in element:
-                    if type(number) is int or type(number) is float:
-                        return
-                    else:
-                        raise TypeError(
-                            "matrix must be a matrix (list of lists) of integers/floats")
-            new_list = (list(map((lambda columna: list(map((lambda x:
-                                                            round(x/div, 2)), columna))), matrix)))
-    else:
+    """
+    give a number with your respective number to divide
+    """
+    e = 'matrix must be a matrix(list of lists) of integers/floats'
+    if type(matrix) is not list or len(matrix) == 0 or type(matrix[0])\
+            is not list:
         raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats")
+            "matrix must be a matrix(list of lists) of integers/floats")
+    if type(div) is not float and type(div) is not int:
+        raise TypeError("div must be a number")
+    if (div == 0):
+        raise ZeroDivisionError("division by zero")
+    size = len(matrix[0])
+    for columna in matrix:
+        if size != len(columna):
+            raise TypeError(
+                "Each row of the matrix must have the same size")
+        if type(columna) is not list:
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
+        for numero in columna:
+            if type(numero) is not float and type(numero) is not int:
+                raise TypeError(e)
+    new_list = (list(map((lambda columna: list(map((lambda x:
+                round(x/div, 2)), columna))), matrix)))
     return(new_list)
