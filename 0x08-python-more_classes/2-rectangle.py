f28e76fd46c2cb945e856f5f060b5adf263c224c
daniela2001-png <dmbmora@outlook.com>
2020-09-28 18:35:05 -0500

    adding file two

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -13,8 +13,8 @@ class Rectangle:
         """
         init the objects in the constructor
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -39,17 +39,17 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """process the value to get """
-        if type(value) is not int:
+        if (type(value) is not int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        elif (value < 0):
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
 
     def area(self):
         """method that returns the area of a rectangle"""
-        return (self.__width * self.__height)
+        return ((self.__width) * (self.__height))
 
     def perimeter(self):
         """returns the perimter of a rectangle"""
-        return ((2*self.__width) + (2*self.__height))
+        return ((2 * self.__width) + (2 * self.__height))
