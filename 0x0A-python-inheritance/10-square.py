9495413a82592d8373894387d6d0b2e28a2ec07f
daniela2001-png <dmbmora@outlook.com>
2020-10-06 16:50:09 -0500

    adding file number 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+clase square que hereda de recatngle
+importamos nuestra clase padre Rectangle
+
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """constructor  d emi clase hija"""
+    def __init__(self, size):
+        """
+        privado atributo size
+        """
+        super().__init__(size, size)
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """area de square"""
+        return self.__size ** 2
