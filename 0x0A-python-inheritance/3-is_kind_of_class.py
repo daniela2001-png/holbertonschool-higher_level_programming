fd5f09b943fad4d4fc5e8d54b19501ebcb12ebbb
daniela2001-png <dmbmora@outlook.com>
2020-10-06 14:26:01 -0500

    adding file number 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+
+funcion que retorna un bool
+
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+
+    retorna true o false si es o no un objeto de la clase
+
+    """
+    return isinstance(obj, a_class)
