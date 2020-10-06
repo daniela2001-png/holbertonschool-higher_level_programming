d3ffa37146668afe2383725a6798206b4e9e1c2c
daniela2001-png <dmbmora@outlook.com>
2020-10-06 18:27:19 -0500

    adding file 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,15 +9,12 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """constructor  d emi clase hija"""
+    """ Class Square """
     def __init__(self, size):
-        """
-        privado atributo size
-        """
-        super().__init__(size, size)
-        super().integer_validator("size", size)
+        self.integer_validator('size', size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
-        """area de square"""
-        return (self.__size ** 2)
+        """ method area """
+        return self.__size ** 2
