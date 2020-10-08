9da848176fe9ecd37ea996bb2e7a2bd33c9d6e14
daniela2001-png <dmbmora@outlook.com>
2020-10-07 23:17:03 -0500

    adding README

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,6 +8,6 @@ def read_file(filename=""):
     """
     function that return a filename with meta utf8
     """
-    with open(filename, encoding='utf8') as my_file:
+    with open(filename, 'r', encoding='utf8') as my_file:
         my_file_read = my_file.read()
         print(my_file_read)
