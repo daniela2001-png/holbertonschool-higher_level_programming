6163e97d95b1ba3aa973b56224c1f749647c1d65
daniela2001-png <dmbmora@outlook.com>
2020-10-08 01:49:11 -0500

    adding 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -7,6 +7,7 @@ a script that adds all arguments to a Python list, and then save them to a file
 
 
 from sys import argv
+
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
@@ -20,4 +21,4 @@ except Exception as e:
     lista = []
 
 """aqui escribo sobre la lista sin modificarla"""
-save_to_json_file(argv[1:]+lol, filename)
+save_to_json_file(argv[1:] + lol, filename)
