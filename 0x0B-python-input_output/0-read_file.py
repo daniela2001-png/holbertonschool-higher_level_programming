be866e072bfc0f9edc985b9691386dcbc924bdd1
daniela2001-png <dmbmora@outlook.com>
2020-10-07 23:14:03 -0500

    adding file 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Write a function that reads a text file (UTF8) and prints it to stdout:
+"""
+
+
+def read_file(filename=""):
+    """
+    function that return a filename with meta utf8
+    """
+    with open(filename, encoding='utf8') as my_file:
+        my_file_read = my_file.read()
+        print(my_file_read)
