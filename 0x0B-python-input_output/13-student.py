d88ac63ac5255e90c67f01f6ce946c0c2e2577a5
daniela2001-png <dmbmora@outlook.com>
2020-10-08 03:37:16 -0500

    adding 13

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""
+Write a class Student that defines a student
+"""
+
+
+class Student:
+    """
+    define  a class student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        define my constructor
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        return dict of my object created
+        """
+        if attrs is not None:
+            my_dict = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    my_dict[i] = self.__dict__[i]
+            return my_dict
+        else:
+            return(self.__dict__)
+
+    def reload_from_json(self, json):
+        """
+        replaces all attributes of the Student instance:
+
+        """
+        if json:
+            self.__dict__ = json
