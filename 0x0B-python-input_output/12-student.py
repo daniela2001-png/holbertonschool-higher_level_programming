9360d97ac2c403c556b9354f3de9fa50cec1adf0
daniela2001-png <dmbmora@outlook.com>
2020-10-08 03:09:09 -0500

    adding 12

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+Write a class Student that defines a student
+"""
+
+
+class Student:
+    """
+    define  a class student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        define my constructor
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        return dict of my object created
+        """
+        my_dict = {}
+        if attrs:
+            for i in attrs:
+                if i in self.__dict__:
+                    my_dict[i] = self.__dict__[i]
+            return my_dict
+        return(self.__dict__)
