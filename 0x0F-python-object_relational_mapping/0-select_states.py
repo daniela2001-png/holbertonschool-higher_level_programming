6d82f5633155ceab7e922aa86c22d8182102645a
daniela2001-png <dmbmora@outlook.com>
2020-12-11 10:41:59 -0500

    [new] USING MYSQLDB WITH PYTHON

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+'''
+este modulo de python ejecuta mysqldb
+para hacer querys y ejecutarlos desde un script
+espero no sé me dificulte entender esto despues de 4 meses :v
+'''
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+    ''' La secuencia de comandos debe tomar 3 argumentos: mysql username,\
+        mysql password y databasename'''
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    # hacemos el query SQL a nuetsra tabla state
+    c.execute("""SELECT * FROM states
+                ORDER BY id ASC""")
+    # obtenemos todos los objetos del query
+    for row in c.fetchall():
+        print(row)
+
+    '''
+    Este método cierra el cursor y db,
+    restablece todos los resultados y garantiza
+    que el objeto del cursor no tenga ninguna referencia
+    a su objeto de conexión original.
+    '''
+    c.close()
+    db.close()
