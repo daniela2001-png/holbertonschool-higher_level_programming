f6709d8e4ca1fa3034df419ab4173a77924de1dc
daniela2001-png <dmbmora@outlook.com>
2020-12-12 16:41:35 -0500

    [add] adding point 13 delete method

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+enumere todos los State objetos de la base de datoshbtn_0e_6_usa
+'''
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+import sys
+if __name__ == "__main__":
+    '''
+    en este punto creamos el engine a nuetsra base de datos mysqldb
+    tomado el input con argv[] y creamos el session instance
+    '''
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    # creamos nuestro objeto sesisonmarker
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter(State.name.like('%a%')).all()
+    # sacamos el objeto de la lista de objetos y pa fuera papu
+    for row in state:
+        session.delete(row)
+    session.commit()
+    session.close()
