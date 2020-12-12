fc6523f1fa60bbd4c46c872e2352665f90a1c273
daniela2001-png <dmbmora@outlook.com>
2020-12-12 17:42:59 -0500

    [add] adding point 14 join tables in sqlalchemy 1.3

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+contenga la definición de clase de a State
+y una instancia Base = declarative_base()
+
+'''
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    '''
+    la clase City al igual que le resto heredaran
+    de Base
+    '''
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('state.id'), nullable=False)
