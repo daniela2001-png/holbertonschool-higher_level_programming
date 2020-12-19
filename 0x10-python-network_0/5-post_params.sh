9cdf557474aa01c496f9f32b4f83c5fa50b78618
daniela2001-png <dmbmora@outlook.com>
2020-12-19 17:37:45 -0500

    doing post

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # post data with curl
-curl -X POST "$1" -d 'email=hr@holbertonschool.com&subject=I will always be here for PLD'
+curl -s -X POST "$1" -d 'email=hr@holbertonschool.com&subject=I will always be here for PLD'
