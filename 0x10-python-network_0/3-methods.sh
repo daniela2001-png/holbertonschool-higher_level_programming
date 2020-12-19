faae0ba70e482bf1a1575b415bf9821c218ca4d3
daniela2001-png <dmbmora@outlook.com>
2020-12-19 16:50:23 -0500

    point three liste the http verbs allowed by the server ubuntu

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Write a Bash script that takes in a URL and displays all HTTP methods the server will accept.
-# use grep cuz con tail y head seme complicaba la cosa
-curl -si "$1" | grep "Allow" | cut -d " " -f 2-
+# I - Fetch the headers only! HTTP-servers feature the command HEAD which this uses to get nothing but the header of a document.
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
