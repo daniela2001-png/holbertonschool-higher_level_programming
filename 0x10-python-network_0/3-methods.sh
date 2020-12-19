e177ddf991259a6103ccc3324fdae169e6abc2dc
daniela2001-png <dmbmora@outlook.com>
2020-12-19 17:13:42 -0500

    point three liste the http verbs allowed by the server ubuntu

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# -i --include this give me the response! vs -I --head include just the header not response!
 # Script that takes in a URL and displays all HTTP methods the server will accept
 curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
