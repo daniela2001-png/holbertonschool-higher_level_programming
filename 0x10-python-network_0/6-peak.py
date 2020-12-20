0bd4e5e8d0b196af1e06cd769ee4231ef57ab720
daniela2001-png <dmbmora@outlook.com>
2020-12-19 19:59:23 -0500

    find  a peak in a list

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,6 +4,9 @@ in other way :() """
 
 
 def find_peak(list_of_integers):
+    """
+    function that founds the peak in a list
+    """
     if list_of_integers:
         list_of_integers.sort()
         return list_of_integers[-1]
