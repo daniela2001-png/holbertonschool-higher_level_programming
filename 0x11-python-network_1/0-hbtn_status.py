ddda3398f205143b5e159c2964d3dff3248051c9
daniela2001-png <dmbmora@outlook.com>
2021-01-12 15:38:04 -0500

    [Add] Addinf point zero request to intranet.io

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,7 @@ from urllib import request
 
 with request.urlopen('https://intranet.hbtn.io/status') as response:
     result = response.read()
-    h = "Body Response:\n\t" \
+    h = "Body response:\n\t" \
         "- type: {}\n\t" \
         "- content: {}\n\t" \
         "- utf8 content: {}"
