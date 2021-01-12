2d68005391efe85d09232b954a8e08367ea2af0e
daniela2001-png <dmbmora@outlook.com>
2021-01-12 16:09:48 -0500

    [Add] Adding point one

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-from urllib import request
-from sys import argv
 """
 request url intranet with argv
 using the module urllib
 """
+
+
+from urllib import request
+from sys import argv
+
 with request.urlopen(argv[1]) as response:
     result = response.info()
     print(result.get("X-Request-Id"))
