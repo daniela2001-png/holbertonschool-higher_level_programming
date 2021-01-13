41bde1b3937bff33d916da393b032031ec56e8d8
daniela2001-png <dmbmora@outlook.com>
2021-01-13 10:05:45 -0500

    [Add] adding point five using module requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,6 +7,7 @@ using the module requests
 if __name__ == "__main__":
     import requests
     response = requests.get("https://intranet.hbtn.io/status")
+    # al checker no le gusta esto :v
     # print('Body response:\n'f'\t- type: {type(response.content.decode())}'
     #       '\n'f'\t- content: {response.content.decode()}')
     h = "Body response:\n\t" \
