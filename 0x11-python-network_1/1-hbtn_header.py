bde963ef7bad99af3ca928bb79fdfcbdc0f4f464
daniela2001-png <dmbmora@outlook.com>
2021-01-13 08:44:29 -0500

    [Add] changes adding

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,7 +5,7 @@ using the module urllib
 """
 from urllib import request
 from sys import argv
-
-with request.urlopen(argv[1]) as response:
-    result = response.info()
-    print(result.get("X-Request-Id"))
+if __name__ == "__main__":
+    with request.urlopen(argv[1]) as response:
+        result = response.info()
+        print(result.get("X-Request-Id"))
