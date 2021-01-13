e71fddc6a73e5820edca318810c14605f5ff66e6
daniela2001-png <dmbmora@outlook.com>
2021-01-13 12:46:42 -0500

    [Add] adding point ten request api github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,9 +6,9 @@ Do requests to API github
 from requests.auth import HTTPBasicAuth
 import requests
 from sys import argv
+if __name__ == "__main__":
+    response = requests.get(
+        "https://api.github.com/users/{}".format(argv[1]),
+        auth=HTTPBasicAuth(argv[1], argv[2]))
 
-response = requests.get(
-    "https://api.github.com/users/{}".format(argv[1]),
-    auth=HTTPBasicAuth(argv[1], argv[2]))
-
-print(response.json().get("id"))
+    print(response.json().get("id"))
